f744a2d7cd70848af014ca801ac02351de932ea9
takot <takot@DESKTOP-LPSKTI6>
2022-04-29 13:28:27 +0900

    adsfada

--- MecaMod/src/main/java/meca/MecaMod.java
+++ MecaMod/src/main/java/meca/MecaMod.java
@@ -22,6 +22,8 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @Mod(modid = MecaMod.MODID, name = MecaMod.NAME, version = MecaMod.VERSION)
 public class MecaMod {
 
+	//aaa
+
 	 	public static final String MODID = "mecamod";
 	    public static final String NAME = "MecaMod";
 	    public static final String VERSION = "1.0";
